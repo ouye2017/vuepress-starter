32778b8a0b93173b0402e5fa2052a2225a70052a
ouye2017 <944528607@qq.com>
2022-08-14 14:32:02 +0800

    feat:     增加PWA

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:ouye2017/vuepress-starter.git tangtang:gh-pages
+git push -f git@github.com:ouye2017/vuepress-starter.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,16 +2,61 @@ module.exports = {
   base: "/vuepress-starter/",
   title: 'Hello VuePress',
   description: 'Just playing around',
+  plugins: [
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "内容偷偷更新了.",
+          buttonText: "刷新"
+        }
+      }
+    ]
+  ],
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png'
+      }
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: ['@vuepress/pwa', {
+    serviceWorker: true,
+    updatePopup: true
+  }],
 
   themeConfig: {
     logo: '/assets/img/logo.jpg',
     sidebar: 'auto',
-	nav: [
+    nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
       { text: 'External', link: 'https://www.baidu.com/' },
     ],
-   //多个侧边栏方式
+    //多个侧边栏方式
     sidebar: {
       '/foo/': [
         '',     /* /foo/ */
@@ -30,7 +75,7 @@ module.exports = {
         '',        /* / */
         'contact', /* /contact.html */
         'about'    /* /about.html */
-    
+
       ]
     }
     // 侧边栏方式
